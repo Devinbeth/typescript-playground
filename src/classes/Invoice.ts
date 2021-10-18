9a358c4c98bc23e31e0b60ec740454ed86d990e0
Devin Lethbridge <devinbeth@gmail.com>
2021-10-18 18:51:33 -0400

    more playing

--- src/classes/Invoice.ts
+++ src/classes/Invoice.ts
@@ -10,6 +10,6 @@ export class Invoice {
   ){}
 
   format() {
-    return `${this.client} owes £${this.amount} for ${this.details}`;
+    return `${this.client} owes $${this.amount} for ${this.details}`;
   }
 }
